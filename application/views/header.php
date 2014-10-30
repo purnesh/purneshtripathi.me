8330ed36efcb358de22b34400b460e90cdaf2f59
purnesh <purnesh.xyz@gmail.com>
2014-10-30 16:43:06 +0530

    Basic Integration with CI completed

--- application/views/header.php
+++ application/views/header.php
@@ -29,21 +29,19 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="This is a portfolio for Purnesh Tripathi" />
 		<meta name="keywords" content="purnesh, tripathi, programmer, Rishikesh, Dehradun, India, Blogger" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/jquery.scrolly.min.js"></script>
-		<script src="js/jquery.onvisible.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-			<link rel="stylesheet" href="css/style-noscript.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="<?php echo base_url("content/css");?>/ie/html5shiv.js"></script><![endif]-->
+		<script src="<?php echo base_url('content/js');?>/jquery.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/jquery.dropotron.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/jquery.scrolly.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/jquery.onvisible.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/skel.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/skel-layers.min.js"></script>
+		<script src="<?php echo base_url("content/js");?>/init.js"></script>
+			<link rel="stylesheet" href="<?php echo base_url("content/css");?>/skel.css" />
+			<link rel="stylesheet" href="<?php echo base_url("content/css");?>/style.css" />
+			<link rel="stylesheet" href="<?php echo base_url("content/css");?>/style-desktop.css" />
+			<link rel="stylesheet" href="<?php echo base_url("content/css");?>/style-noscript.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url("content/css");?>/ie/v8.css" /><![endif]-->
 	</head>
 	<body class="homepage">
 
