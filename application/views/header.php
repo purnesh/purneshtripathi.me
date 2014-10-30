0ed7a30a39828fe4957e030b98f990a511ca1a66
purnesh <purnesh.xyz@gmail.com>
2014-10-30 16:08:12 +0530

    Adding remote Github repository

---
+++ application/views/header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<!--
+
+   ▄███████▄ ███    █▄     ▄████████ ███▄▄▄▄      ▄████████    ▄████████    ▄█    █▄    
+  ███    ███ ███    ███   ███    ███ ███▀▀▀██▄   ███    ███   ███    ███   ███    ███   
+  ███    ███ ███    ███   ███    ███ ███   ███   ███    █▀    ███    █▀    ███    ███   
+  ███    ███ ███    ███  ▄███▄▄▄▄██▀ ███   ███  ▄███▄▄▄       ███         ▄███▄▄▄▄███▄▄ 
+▀█████████▀  ███    ███ ▀▀███▀▀▀▀▀   ███   ███ ▀▀███▀▀▀     ▀███████████ ▀▀███▀▀▀▀███▀  
+  ███        ███    ███ ▀███████████ ███   ███   ███    █▄           ███   ███    ███   
+  ███        ███    ███   ███    ███ ███   ███   ███    ███    ▄█    ███   ███    ███   
+ ▄████▀      ████████▀    ███    ███  ▀█   █▀    ██████████  ▄████████▀    ███    █▀    
+                          ███    ███                                                    
+    ███        ▄████████  ▄█     ▄███████▄    ▄████████     ███        ▄█    █▄     ▄█  
+▀█████████▄   ███    ███ ███    ███    ███   ███    ███ ▀█████████▄   ███    ███   ███  
+   ▀███▀▀██   ███    ███ ███▌   ███    ███   ███    ███    ▀███▀▀██   ███    ███   ███▌ 
+    ███   ▀  ▄███▄▄▄▄██▀ ███▌   ███    ███   ███    ███     ███   ▀  ▄███▄▄▄▄███▄▄ ███▌ 
+    ███     ▀▀███▀▀▀▀▀   ███▌ ▀█████████▀  ▀███████████     ███     ▀▀███▀▀▀▀███▀  ███▌ 
+    ███     ▀███████████ ███    ███          ███    ███     ███       ███    ███   ███  
+    ███       ███    ███ ███    ███          ███    ███     ███       ███    ███   ███  
+   ▄████▀     ███    ███ █▀    ▄████▀        ███    █▀     ▄████▀     ███    █▀    █▀   
+              ███    ███                                                                
+
+	Purnesh Tripathi <purnesh.xyz@gmail.com>
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Purnesh Tripathi - Portfolio</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="This is a portfolio for Purnesh Tripathi" />
+		<meta name="keywords" content="purnesh, tripathi, programmer, Rishikesh, Dehradun, India, Blogger" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.onvisible.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+			<link rel="stylesheet" href="css/style-noscript.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<!-- Header -->
+			<div id="header">
+						
+				<!-- Inner -->
+					<div class="inner">
+						<header>
+							<h1><a href="index.html" id="logo">Purnesh Tripathi</a></h1>
+							<hr />
+							<p>You are born with nothing, but you die with a name.
+							<br /> So that name should not be just a name, it should be a <br />
+							LEGEND!</p>
+						</header>
+						<footer>
+							<a href="#banner" class="button circled scrolly">Begin!</a>
+						</footer>
+					</div>
+				
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="left-sidebar.html">About Me</a></li>
+							<li>
+								<a href="right-sidebar.html">Projects</a>
+								<ul>
+									<li><a href="#">Lorem ipsum dolor</a></li>
+									<li><a href="#">Magna phasellus</a></li>
+									<li><a href="#">Etiam dolore nisl</a></li>
+									<li>
+										<a href="">And a submenu &hellip;</a>
+										<ul>
+											<li><a href="#">Lorem ipsum dolor</a></li>
+											<li><a href="#">Phasellus consequat</a></li>
+											<li><a href="#">Magna phasellus</a></li>
+											<li><a href="#">Etiam dolore nisl</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Veroeros feugiat</a></li>
+								</ul>
+							</li>
+							<li><a href="no-sidebar.html">Contact Me</a></li>
+						</ul>
+					</nav>
+
+			</div>
+			
